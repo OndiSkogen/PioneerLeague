0078196d3505523e0c99d0953e576af980db025d
OndiSkogen <fiskkneipp@gmail.com>
2022-10-24 02:15:57 +0200

    Showing tooltip on hover

--- PioneerLiganSTHLM/wwwroot/js/site.js
+++ PioneerLiganSTHLM/wwwroot/js/site.js
@@ -18,3 +18,6 @@ function Collapser(item) {
 
     }
 }
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
